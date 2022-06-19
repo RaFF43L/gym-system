2e17c2232ac0e281e9a3dd0f90d6e0d987cbc64e
Raffael Pimentel Ribeiro <ribeiro_pimentel@discente.ufg.br>
2022-06-19 02:41:55 -0300

    ajudantando gitignore

--- backend/database/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require("sequelize");
-
-const connection = new Sequelize('filmaria','postgres', '96340213',{
-    host: 'localhost',
-    dialect: 'postgres'
-});
-
-module.exports = connection;
